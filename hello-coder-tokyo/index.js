d9a3b1584fb95b49771bbd63f4d1ed505ee389ba
thanhnh <thanhnh99.amc@gmail.com>
2020-04-27 17:25:35 +0700

    express hello coder-tokyo

---
+++ hello-coder-tokyo/index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+var port=3000;
+
+app.get("/",function(req,res)
+{
+    res.send("<h1>Hello Coder-Tokyo");
+});
+
+app.listen(port,function()
+{
+    console.log("Server is listenning on port "+port);
+})
