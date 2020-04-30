d8e87b9495de7fffffbfbc9d4618fd4d1d0bf3fd
thanhnh <thanhnh99.amc@gmail.com>
2020-05-01 04:58:08 +0700

    paging products

--- hello-coder-tokyo/controllers/product.controller.js
+++ hello-coder-tokyo/controllers/product.controller.js
@@ -2,13 +2,23 @@ var db = require("../db")
 
 module.exports.viewProduct = function(req,res)
 {
-    var page = req.query.page || 1;
+    var page = parseInt(req.query.page) || 1;
     var perPage = 9
 
     var start = (page-1)*perPage;
     var end = page*perPage;
     var products = db.get("products").value().slice(start,end);
+
+    var paging = [];
+    
+    paging.push(page-1);
+    paging.push(page);
+    paging.push(page+1);
+    console.log(paging)
+
+
     res.render("product/product",{
-        products:products
+        products:products,
+        paging:paging
     })
 }
